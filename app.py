654d0e12830e5cd758256e59e3107e95474ab4f1
BBaoVanC <github@bbaovanc.com>
2021-01-25 01:12:55 -0600

    Add fancy and discord urls

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ The main Flask webapp
 """
 
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, redirect, render_template
 from flask_api import status
 
 import encoding
+import settings
 
 
 app = Flask(__name__)
@@ -25,7 +26,28 @@ def decode(filename):
     try:
         return encoding.decode(filename)
     except encoding.EncodeDecodeError:
+        print("filename doesn't contain only 200b and 200c")
         return jsonify({'status': 'error', 'error': 'INCORRECTLY_ENCODED'}), status.HTTP_400_BAD_REQUEST
+
+
+@app.route("/v1/fancy/<image>", methods = ["GET"])
+def fancy(image):
+    try:
+        decimg = encoding.decode(image)
+    except encoding.EncodeDecodeError:
+        print("filename doesn't contain only 200b and 200c")
+        return jsonify({'status': 'error', 'error': 'INCORRECTLY_ENCODED'}), status.HTTP_400_BAD_REQUEST
+    return redirect(settings.ROOTURL + decimg, 307)
+
+
+@app.route("/v1/discord/<image>", methods = ["GET"])
+def discord(image):
+    try:
+        decimg = encoding.decode(image)
+    except encoding.EncodeDecodeError:
+        print("filename doesn't contain only 200b and 200c")
+        return jsonify({'status': 'error', 'error': 'INCORRECTLY_ENCODED'}), status.HTTP_400_BAD_REQUEST
+    return render_template("discord.html", url = settings.ROOTURL + decimg)
 
 
 if __name__ == "__main__":
